694973979a04c044151d0239daa542f776685161
Samuel Colvin <s@muelcolvin.com>
2016-10-04 12:17:08 +0100

    Adding jinja type
    see #145 discussion.

--- src/types.rs
+++ src/types.rs
@@ -82,6 +82,7 @@ const TYPE_EXTENSIONS: &'static [(&'static str, &'static [&'static str])] = &[
     ("xml", &["*.xml"]),
     ("yacc", &["*.y"]),
     ("yaml", &["*.yaml", "*.yml"]),
+    ("jinja", &["*.jinja", "*.jinja2"]),
 ];
 
 /// Describes all the possible failure conditions for building a file type
